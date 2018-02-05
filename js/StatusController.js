67e5db5aa9cf17385c7cafe9a8ab643272a66a40
Joshua Shane Martin <josmar52789@gmail.com>
2018-02-05 13:13:30 -0500

    Adding current punch

--- js/StatusController.js
+++ js/StatusController.js
@@ -1,14 +1,21 @@
 app.controller('StatusController', function ($rootScope, $scope, $window) {
     $scope.refreshStatus = function () {
-        db.collection('punches').find({ user_id: client.authedId(), out_at: { $exists: false } }).sort({ in_at: -1 }).limit(1).execute()
-            .then(punches => {
-                if (punches.length > 0) {
-                    $rootScope.current.punch = punches[0]
-                    $rootScope.$apply()
-                }
-            }).catch(error => {
+        $http.get("odata/Punches/Default.Current")
+            .then(response => {
+                $rootScope.current.punch = response
+            }, error => {
                 console.error(error)
             })
+
+        // db.collection('punches').find({ user_id: client.authedId(), out_at: { $exists: false } }).sort({ in_at: -1 }).limit(1).execute()
+        //     .then(punches => {
+        //         if (punches.length > 0) {
+        //             $rootScope.current.punch = punches[0]
+        //             $rootScope.$apply()
+        //         }
+        //     }).catch(error => {
+        //         console.error(error)
+        //     })
     };
 
     $scope.refreshStatus();
