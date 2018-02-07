58fe107ef0b2956457d03f2383daf2cafade8ea0
Joshua Shane Martin <josmar52789@gmail.com>
2018-02-07 17:42:33 -0500

    Adding apply

--- js/InJobController.js
+++ js/InJobController.js
@@ -7,6 +7,7 @@ app.controller('InJobController', function ($http, $location, $rootScope, $scope
         $http.get($rootScope.baseUrl + "odata/Jobs(" + $scope.job.Id + ")")
             .then(response => {
                 $rootScope.selected.job = response
+                $scope.$apply()
                 $location.path('/in/task')
             }, error => {
                 $scope.working.search = false
