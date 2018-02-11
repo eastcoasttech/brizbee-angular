af86b046ceccb76e04920bc5cbcfaf7ee2631620
Joshua Shane Martin <josmar52789@gmail.com>
2018-02-10 20:58:53 -0500

    Fixing reference to value

--- js/PunchesController.js
+++ js/PunchesController.js
@@ -36,7 +36,7 @@ app.controller('PunchesController', function ($http, $rootScope, $scope, $uibMod
         $http.get($rootScope.baseUrl + "odata/Commits?$orderby=InAt desc")
             .then(response => {
                 $scope.loading.commits = false
-                $scope.commits = response.data
+                $scope.commits = response.value
             }, error => {
                 $scope.loading.commits = false
                 console.error(error)
@@ -61,7 +61,7 @@ app.controller('PunchesController', function ($http, $rootScope, $scope, $uibMod
         $http.get($rootScope.baseUrl + "odata/Punches?$expand=User,Task($expand=Job($expand=Customer))")
             .then(response => {
                 $scope.loading.punches = false
-                $scope.punches = response.data
+                $scope.punches = response.value
             }, error => {
                 $scope.loading.punches = false
                 console.error(error)
