e79983fb92fab34a31f72e6062f025fc1253f394
Joshua Shane Martin <josmar52789@gmail.com>
2018-02-12 12:03:37 -0500

    Refactoring names

--- js/RangeController.js
+++ js/RangeController.js
@@ -1,20 +1,20 @@
 app.controller('RangeController', function ($scope, $uibModalInstance, range) {
-    $scope.datepicker = { in_at: {}, out_at: {}, options: {} }
+    $scope.datepicker = { InAt: {}, OutAt: {}, options: {} }
     $scope.range = range
     
     $scope.ok = function () {
         $uibModalInstance.close($scope.range);
-    };
+    }
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
-    };
+    }
     
     $scope.showOutAtDatepicker = function () {
-        $scope.datepicker.out_at.opened = true
-    };
+        $scope.datepicker.OutAt.opened = true
+    }
 
     $scope.showInAtDatepicker = function () {
-        $scope.datepicker.in_at.opened = true
-    };
+        $scope.datepicker.InAt.opened = true
+    }
 });
