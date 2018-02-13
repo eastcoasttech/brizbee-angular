70d63640d91f1b4e89984e008033a6a6e23c6288
Joshua Shane Martin <josmar52789@gmail.com>
2018-02-13 12:05:35 -0500

    Refactoring variables

--- js/PunchDetailsController.js
+++ js/PunchDetailsController.js
@@ -1,24 +1,25 @@
-app.controller('PunchDetailsController', function ($filter, $rootScope, $scope, $uibModalInstance, punch) {
+app.controller('PunchDetailsController', function ($filter, $http, $rootScope, $scope, $uibModalInstance, punch) {
     $scope.customers = []
-    $scope.datepicker = { in_at: {}, out_at: {}, options: {} }
+    $scope.datepicker = { InAt: {}, OutAt: {}, options: {} }
     $scope.jobs = []
     $scope.loading = { customers: false, jobs: false, tasks: false }
-    if (punch._id == null) {
+    if (punch.Id == null) {
         $scope.punch = {
-            in_at: moment().startOf('day').toDate(),
-            out_at: moment().endOf('day').toDate(),
+            InAt: moment().startOf('day').toDate(),
+            OutAt: moment().endOf('day').toDate(),
             has_out_at: true
         }
     } else {
         $scope.punch = punch
-        if ($scope.punch.out_at != null) {
+        if ($scope.punch.OutAt != null) {
             $scope.punch.has_out_at = true
         }
     }
+    $scope.tasks = []
     $scope.working = { save: false }
 
     $scope.save = function () {
-        if (punch._id == null) {
+        if (punch.Id == null) {
             $scope.saveNewPunch()
         } else {
             $scope.saveExistingPunch()
@@ -26,96 +27,73 @@ app.controller('PunchDetailsController', function ($filter, $rootScope, $scope, 
     };
 
     $scope.saveNewPunch = function () {
-        var punch = {
-            owner_id: client.authedId(),
-            organization_id: $rootScope.current.user.organization_id,
-            in_at: $scope.punch.in_at,
-            customer_id: $scope.punch.customer._id,
-            customer_name: $scope.punch.customer.name,
-            customer_number: $scope.punch.customer.number,
-            job_id: $scope.punch.job._id,
-            job_name: $scope.punch.job.name,
-            job_number: $scope.punch.job.number,
-            task_id: $scope.punch.task._id,
-            task_name: $scope.punch.task.name,
-            task_number: $scope.punch.task.number,
-            user_id: $scope.punch.user._id,
-            user_name: $scope.punch.user.name
+        var json = {
+            InAt: $scope.punch.InAt,
+            TaskId: $scope.punch.task.Id,
+            UserId: $scope.punch.user.Id
         }
 
         // out_at is optional when editing manually
         if ($scope.punch.has_out_at) {
-            punch.out_at = $scope.punch.out_at
+            json.OutAt = $scope.punch.out_at
         }
 
-        db.collection('punches').insertOne(punch)
-            .then(result => {
+        $http.post($rootScope.baseUrl + "odata/Punches", JSON.stringify(json))
+            .then(response => {
                 $scope.ok()
-            })
-            .catch(error => {
+            }, error => {
                 console.error(error)
             })
     };
 
     $scope.saveExistingPunch = function () {
-        var punch = {
-            in_at: $scope.punch.in_at,
-            customer_id: $scope.punch.customer._id,
-            customer_name: $scope.punch.customer.name,
-            customer_number: $scope.punch.customer.number,
-            job_id: $scope.punch.job._id,
-            job_name: $scope.punch.job.name,
-            job_number: $scope.punch.job.number,
-            task_id: $scope.punch.task._id,
-            task_name: $scope.punch.task.name,
-            task_number: $scope.punch.task.number,
-            user_id: $scope.punch.user._id,
-            user_name: $scope.punch.user.name
+        var json = {
+            InAt: $scope.punch.InAt,
+            TaskId: $scope.punch.task.Id,
+            UserId: $scope.punch.user.Id
         }
 
         // out_at is optional when editing manually
         if ($scope.punch.has_out_at) {
-            punch.out_at = $scope.punch.out_at
+            json.OutAt = $scope.punch.out_at
         } else {
-            punch.out_at = null
+            json.OutAt = null
         }
 
-        db.collection('punches').updateOne({ _id: $scope.punch._id }, { $set: punch })
-            .then(result => {
+        $http.put($rootScope.baseUrl + "odata/Punches(" + $scope.punch.Id + ")", JSON.stringify(json))
+            .then(response => {
                 $scope.ok()
-            })
-            .catch(error => {
+            }, error => {
                 console.error(error)
             })
     };
 
     $scope.ok = function () {
-        $uibModalInstance.close('Success');
+        $uibModalInstance.close('Success')
     };
 
     $scope.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
+        $uibModalInstance.dismiss('cancel')
+    }
 
     $scope.refreshCustomers = function () {
         $scope.customers = []
         $scope.loading.customers = true
-        db.collection('customers').find().sort({ number: 1 }).execute()
-            .then(docs => {
+        $http.get($rootScope.baseUrl + "odata/Customers?$orderby=Id")
+            .then(response => {
                 $scope.loading.customers = false
-                $scope.customers = docs
-                if (!$scope.punch.customer_id) {
+                $scope.customers = response.data.value
+                if (!$scope.punch.customer) {
                     $scope.punch.customer = $scope.customers[0]
                 } else {
-                    $scope.punch.customer = $filter('filter')($scope.customers, { _id: $scope.punch.customer_id }, true)[0]
+                    $scope.punch.customer = $filter('filter')($scope.customers, { Id: $scope.punch.customer.Id }, true)[0]
                 }
-                $scope.$apply()
                 $scope.refreshJobs()
-            }).catch(err => {
+            }, error => {
                 $scope.loading.customers = false
-                console.error(err)
+                console.error(error)
             })
-    };
+    }
 
     $scope.refreshJobs = function () {
         if ($scope.punch.customer == null)
@@ -126,20 +104,19 @@ app.controller('PunchDetailsController', function ($filter, $rootScope, $scope, 
         $scope.jobs = []
         $scope.tasks = []
         $scope.loading.jobs = true
-        db.collection('jobs').find({ customer_id: $scope.punch.customer._id }).sort({ number: 1 }).execute()
-            .then(docs => {
+        $http.get($rootScope.baseUrl + "odata/Jobs?$orderby=Id&$filter=CustomerId eq " + $scope.punch.customer.Id)
+            .then(response => {
                 $scope.loading.jobs = false
-                $scope.jobs = docs
-                if (!$scope.punch.job_id) {
+                $scope.jobs = response.data.value
+                if (!$scope.punch.job) {
                     $scope.punch.job = $scope.jobs[0]
                 } else {
-                    $scope.punch.job = $filter('filter')($scope.jobs, { _id: $scope.punch.job_id }, true)[0]
+                    $scope.punch.job = $filter('filter')($scope.jobs, { Id: $scope.punch.job.Id }, true)[0]
                 }
-                $scope.$apply()
                 $scope.refreshTasks()
-            }).catch(err => {
+            }, error => {
                 $scope.loading.jobs = false
-                console.error(err)
+                console.error(error)
             })
     };
 
@@ -150,40 +127,38 @@ app.controller('PunchDetailsController', function ($filter, $rootScope, $scope, 
 
         $scope.tasks = []
         $scope.loading.tasks = true
-        db.collection('tasks').find({ job_id: $scope.punch.job._id }).sort({ number: 1 }).execute()
-            .then(docs => {
+        $http.get($rootScope.baseUrl + "odata/Tasks?$orderby=Id&$filter=JobId eq " + $scope.punch.job.Id)
+            .then(response => {
                 $scope.loading.tasks = false
-                $scope.tasks = docs
-                if (!$scope.punch.task_id) {
+                $scope.tasks = response.data.value
+                if (!$scope.punch.task) {
                     $scope.punch.task = $scope.tasks[0]
                 } else {
-                    $scope.punch.task = $filter('filter')($scope.tasks, { _id: $scope.punch.task_id }, true)[0]
+                    $scope.punch.task = $filter('filter')($scope.tasks, { Id: $scope.punch.task.Id }, true)[0]
                 }
-                $scope.$apply()
-            }).catch(err => {
+            }, error => {
                 $scope.loading.tasks = false
-                console.error(err)
+                console.error(error)
             })
-    };
+    }
 
     $scope.refreshUsers = function () {
         $scope.users = []
         $scope.loading.users = true
-        db.collection('users').find().sort({ name: 1 }).execute()
-            .then(docs => {
+        $http.get($rootScope.baseUrl + "odata/Users?$orderby=Id")
+            .then(response => {
                 $scope.loading.users = false
-                $scope.users = docs
-                if (!$scope.punch.user_id) {
+                $scope.users = response.data.value
+                if (!$scope.punch.user) {
                     $scope.punch.user = $scope.users[0]
                 } else {
-                    $scope.punch.user = $filter('filter')($scope.users, { _id: $scope.punch.user_id }, true)[0]
+                    $scope.punch.user = $filter('filter')($scope.users, { Id: $scope.punch.user.Id }, true)[0]
                 }
-                $scope.$apply()
-            }).catch(err => {
+            }, error => {
                 $scope.loading.users = false
-                console.error(err)
+                console.error(error)
             })
-    };
+    }
     
     $scope.showInAtDatepicker = function () {
         $scope.datepicker.in_at.opened = true
