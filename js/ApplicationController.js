b6fb618a06d53bdaeaddf438a5aa561271b0e3ae
Joshua Shane Martin <josmar52789@gmail.com>
2018-02-17 11:57:54 -0500

    Changing root url

--- js/ApplicationController.js
+++ js/ApplicationController.js
@@ -1,5 +1,6 @@
 app.controller('ApplicationController', function ($location, $rootScope, $scope) {
-    $rootScope.baseUrl = "http://localhost:54313/"
+    // $rootScope.baseUrl = "http://localhost:54313/"
+    $rootScope.baseUrl = "https://brizbeeweb.azurewebsites.net"
     $rootScope.selected = {}
     $rootScope.current = {}
     $rootScope.range = { in_at: moment().startOf('day').toDate(), out_at: moment().endOf('day').toDate() }
