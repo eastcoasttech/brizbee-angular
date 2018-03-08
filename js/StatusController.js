26bf8c6dd8e0df06889c81851b0261b2ba458fad
Joshua Shane Martin <josmar52789@gmail.com>
2018-03-08 14:19:20 -0500

    Adding indicator

--- js/StatusController.js
+++ js/StatusController.js
@@ -1,10 +1,15 @@
 app.controller('StatusController', function ($http, $rootScope, $scope, $window) {
+    $scope.working = { status: true }
+
     $scope.refreshStatus = function () {
+        $scope.working.status = true
+
         $http.get($rootScope.baseUrl + "odata/Punches/Default.Current?$expand=Task($expand=Job($expand=Customer))")
             .then(response => {
                 if (response.data != null)
                 {
                     $rootScope.current.punch = response.data
+                    $scope.working.status = false
                 }
             }, error => {
                 console.error(error)
