2a3bf7455a5f1d7683cdb701601d96af034a3a1e
Joshua Shane Martin <josmar52789@gmail.com>
2018-03-27 16:12:48 -0400

    Fixing scroll number

--- js/Application.js
+++ js/Application.js
@@ -1,14 +1,6 @@
 // Disable mousewheel on a input number field when in focus
 // (to prevent Cromium browsers change the value when scrolling)
-$('form').on('focus', 'input[type=number]', function (e) {
-    $(this).on('mousewheel.disableScroll', function (e) {
-        e.preventDefault()
-    })
-})
-
-$('form').on('blur', 'input[type=number]', function (e) {
-    $(this).off('mousewheel.disableScroll')
-})
+$(':input[type=number]').on('mousewheel',function(e){ $(this).blur(); });
 
 // Initialize Angular app
 var app = angular.module('brizby', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
