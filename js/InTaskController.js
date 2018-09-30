21ea89ed689e97f34c27f35f94c7a8e62c4bb420
Joshua Shane Martin <josmar52789@gmail.com>
2018-09-29 21:23:22 -0400

    Fixing task number

--- js/InTaskController.js
+++ js/InTaskController.js
@@ -1,4 +1,5 @@
 app.controller('InTaskController', function ($http, $location, $rootScope, $scope, $window) {
+    $scope.errors = {}
     $scope.task = {}
     $scope.working = {}
 
@@ -6,8 +7,12 @@ app.controller('InTaskController', function ($http, $location, $rootScope, $scop
         $scope.working.search = true
         $http.get($rootScope.baseUrl + "odata/Tasks?$filter=Number eq '" + $scope.task.Number + "'")
             .then(response => {
-                $rootScope.selected.task = response.data.value[0]
-                $location.path('/in/confirm')
+                if (response.data.value.length == 0) {
+                    $scope.errors.task_number_not_found = true;
+                } else {
+                    $rootScope.selected.task = response.data.value[0]
+                    $location.path('/in/confirm')
+                }
             }, error => {
                 $scope.working.search = false
                 console.error(error)
