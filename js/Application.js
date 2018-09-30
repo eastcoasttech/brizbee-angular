5f80eb729f72329d20c55628c36f9a0c78fd72f9
Joshua Shane Martin <josmar52789@gmail.com>
2018-09-29 22:06:24 -0400

    Adding module reference

--- js/Application.js
+++ js/Application.js
@@ -3,7 +3,7 @@
 $(':input[type=number]').on('mousewheel',function(e){ $(this).blur(); });
 
 // Initialize Angular app
-var app = angular.module('brizbee', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies']);
+var app = angular.module('brizbee', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies', 'ui.utils.masks']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
