4f9bf02220e99ed6704a6ef02909c7cb5402242a
Joshua Shane Martin <josmar52789@gmail.com>
2019-01-10 11:42:36 -0500

    Adding filters

--- js/ReportsController.js
+++ js/ReportsController.js
@@ -1,5 +1,40 @@
 app.controller('ReportsController', function ($http, $rootScope, $scope, $window) {
     $scope.datepicker = { in_at: {}, out_at: {}, options: {} }
+    $scope.include = { jobs: { list: [], scope: 'all', search: '' }, users: { list: [], scope: 'all', search: '' } }
+
+    $scope.addUser = function (user) {
+        $scope.include.users.list.push(user)
+    }
+    
+    $scope.removeUser = function (user) {
+        $scope.include.users.list.pop(user)
+    }
+
+    $scope.searchJobs = function () {
+
+    }
+    
+    $scope.searchUsers = function () {
+        $scope.include.users.results = []
+		var result = []
+
+		if ($scope.include.users.search.length < 1) {
+            console.error('No search value provided')
+		} else {
+			$http.get($rootScope.baseUrl + "/odata/Users?$filter=contains(Name,'" + $scope.include.users.search + "')&$select=EmailAddress,Name,Id")
+                .then(function (response)
+                {
+                    if (response.data.value.length >= 1) {
+                        $scope.include.users.results = response.data.value
+                    }
+                }, function(response)
+                {
+                    console.error(response)
+                })
+		}
+
+		return result
+    }
 
     $scope.showOutAtDatepicker = function () {
         $scope.datepicker.out_at.opened = true
