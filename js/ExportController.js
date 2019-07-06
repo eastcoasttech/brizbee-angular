be8f53dadb13a41a3d889d2f388fecb53bb7fe8a
Joshua Shane Martin <josmar52789@gmail.com>
2019-07-05 21:02:17 -0400

    Adding access and refresh tokens

--- js/ExportController.js
+++ js/ExportController.js
@@ -3,6 +3,10 @@ app.controller('ExportController', function ($http, $location, $rootScope, $rout
     $scope.errorMessage = $routeParams.errorMessage
     $scope.stateMessage = $routeParams.stateMessage
     $scope.realmId = $routeParams.realmId
+    $scope.accessToken = $routeParams.accessToken
+    $scope.accessTokenExpiresAt = $routeParams.accessTokenExpiresAt
+    $scope.refreshToken = $routeParams.refreshToken
+    $scope.refreshTokenExpiresAt = $routeParams.refreshTokenExpiresAt
 
     // Scroll to top
     $window.scrollTo(0, 0)
