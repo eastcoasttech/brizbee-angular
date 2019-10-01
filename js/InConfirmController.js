bd18268b3285c618de36735ad9113d63eede404d
Joshua Shane Martin <josmar52789@gmail.com>
2019-10-01 13:19:37 -0400

    Fixing location

--- js/InConfirmController.js
+++ js/InConfirmController.js
@@ -11,48 +11,66 @@ app.controller('InConfirmController', function ($http, $location, $rootScope, $s
 
         // Attempt to get the user's current location
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                latitude = position.coords.latitude
-                longitude = position.coords.longitude
-                console.log(latitude)
-                console.log(longitude)
-                var json = {
-                    InAtTimeZone: $scope.options.InAtTimeZone,
-                    LatitudeForInAt: latitude,
-                    LongitudeForInAt: longitude,
-                    SourceForInAt: 'Web',
-                    TaskId: $rootScope.selected.task.Id
+            navigator.geolocation.getCurrentPosition(
+                currentPositionSuccess,
+                currentPositionFail,
+                // maximumAge refers to the age of the location data in cache,
+                // setting it to infinity guarantees you'll get a cached version.
+                // Setting it to 0 forces the device to retrieve the position.
+                // http://stackoverflow.com/questions/3397585/navigator-geolocation-getcurrentposition-sometimes-works-sometimes-doesnt
+                {
+                    maximumAge: 15000,
+                    timeout: 30000,
+                    enableHighAccuracy: false
                 }
-                $http.post($rootScope.baseUrl + "/odata/Punches/Default.PunchIn", JSON.stringify(json))
-                    .then(response => {
-                        if (response.data != null)
-                        {
-                            $location.path('/in/done')
-                        }
-                    }, error => {
-                        $scope.working.save = false
-                        console.error(error)
-                    })
-            });
+            );
         } else {
-            var json = {
-                InAtTimeZone: $scope.options.InAtTimeZone,
-                LatitudeForInAt: null,
-                LongitudeForInAt: null,
-                SourceForInAt: 'Web',
-                TaskId: $rootScope.selected.task.Id
-            }
-            $http.post($rootScope.baseUrl + "/odata/Punches/Default.PunchIn", JSON.stringify(json))
-                .then(response => {
-                    if (response.data != null)
-                    {
-                        $location.path('/in/done')
-                    }
-                }, error => {
-                    $scope.working.save = false
-                    console.error(error)
-                })
+            save(null, null)
         }
+    }
+
+    function currentPositionSuccess(position) {
+        latitude = position.coords.latitude.toString()
+        longitude = position.coords.longitude.toString()
+        save(latitude, longitude)
+    }
+
+    function currentPositionFail(error) {
+        switch (error.code) {
+            case 0:
+                // UNKNOWN_ERROR
+                break;
+            case 1:
+                // PERMISSION_DENIED
+                break;
+            case 2:
+                // POSITION_UNAVAILABLE
+                break;
+            case 3:
+                // TIMEOUT
+                break;
+        }
+        save(null, null)
+    }
+
+    function save(latitude, longitude) {
+        var json = {
+            InAtTimeZone: $scope.options.InAtTimeZone,
+            LatitudeForInAt: latitude,
+            LongitudeForInAt: longitude,
+            SourceForInAt: 'Web',
+            TaskId: $rootScope.selected.task.Id
+        }
+        $http.post($rootScope.baseUrl + "/odata/Punches/Default.PunchIn", JSON.stringify(json))
+            .then(response => {
+                if (response.data != null)
+                {
+                    $location.path('/in/done')
+                }
+            }, error => {
+                $scope.working.save = false
+                console.error(error)
+            })
     }
 
     // Scroll to top
