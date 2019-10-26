e7bd8d0d3831328ddeb2ff84464314fe5ce8e554
Joshua Shane Martin <josmar52789@gmail.com>
2019-10-26 12:58:11 -0400

    Using latitude and longitude for punch out.

--- js/OutConfirmController.js
+++ js/OutConfirmController.js
@@ -8,8 +8,57 @@ app.controller('OutConfirmController', function ($http, $location, $rootScope, $
 
     $scope.save = function () {
         $scope.working.save = true
-        
-        var json = { SourceForOutAt: 'Web', OutAtTimeZone: $scope.options.OutAtTimeZone }
+
+        // Attempt to get the user's current location
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                currentPositionSuccess,
+                currentPositionFail,
+                // maximumAge refers to the age of the location data in cache,
+                // setting it to infinity guarantees you'll get a cached version.
+                // Setting it to 0 forces the device to retrieve the position.
+                // http://stackoverflow.com/questions/3397585/navigator-geolocation-getcurrentposition-sometimes-works-sometimes-doesnt
+                {
+                    maximumAge: 15000,
+                    timeout: 30000,
+                    enableHighAccuracy: false
+                }
+            );
+        } else {
+            save(null, null)
+        }
+    }
+    function currentPositionSuccess(position) {
+        latitude = position.coords.latitude.toString()
+        longitude = position.coords.longitude.toString()
+        save(latitude, longitude)
+    }
+
+    function currentPositionFail(error) {
+        switch (error.code) {
+            case 0:
+                // UNKNOWN_ERROR
+                break;
+            case 1:
+                // PERMISSION_DENIED
+                break;
+            case 2:
+                // POSITION_UNAVAILABLE
+                break;
+            case 3:
+                // TIMEOUT
+                break;
+        }
+        save(null, null)
+    }
+
+    function save(latitude, longitude) {
+        var json = {
+            SourceForOutAt: 'Web',
+            OutAtTimeZone: $scope.options.OutAtTimeZone,
+            LatitudeForOutAt: latitude,
+            LongitudeForOutAt: longitude
+        }
         $http.post($rootScope.baseUrl + "/odata/Punches/Default.PunchOut", JSON.stringify(json))
             .then(response => {
                 if (response.data != null)
@@ -20,7 +69,7 @@ app.controller('OutConfirmController', function ($http, $location, $rootScope, $
                 $scope.working.save = false
                 console.error(error)
             })
-    };
+    }
 
     // Scroll to top
     $window.scrollTo(0, 0)
