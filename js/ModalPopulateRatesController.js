2459f236d8803ecccca7aa7374911a4b210748f6
Joshua Shane Martin <josmar52789@gmail.com>
2020-03-12 00:14:25 -0400

    Adding service rates.

--- js/ModalPopulateRatesController.js
+++ js/ModalPopulateRatesController.js
@@ -1,42 +1,72 @@
 app.controller('ModalPopulateRatesController', function ($http, $rootScope, $scope, $timeout, $uibModalInstance) {
   $scope.basePayrollRates = [];
+  $scope.baseServiceRates = [];
   $scope.alternatePayrollRates = [];
-  $scope.exceptions = [];
+  $scope.alternateServiceRates = [];
+  $scope.payrollExceptions = [];
+  $scope.serviceExceptions = [];
   $scope.timepicker = { hstep: 1, mstep: 1 };
   $scope.datepicker = {};
 
-  $scope.addException = function () {
-    $scope.exceptions.push({ option: 'After Hours/Minutes in Range', hour: 40, BasePayrollRate: $scope.basePayrollRates[0], Date: new Date() });
+  $scope.addPayrollException = function () {
+    $scope.payrollExceptions.push({ option: 'After Hours/Minutes in Range', hour: 40, BasePayrollRate: $scope.basePayrollRates[0], Date: new Date() });
 
     // Trigger the alternate rate to set default
-    $scope.selectAlternateRate($scope.exceptions[$scope.exceptions.length - 1]);
+    $scope.selectPayrollAlternateRate($scope.payrollExceptions[$scope.payrollExceptions.length - 1]);
 
-    // Allow numbers only, must be applied every time
-    $("input.form-control-number").numeric();
-  };
-
-  $scope.removeException = function (idx) {
-    $scope.exceptions.splice(idx, 1);
-  };
-
-  $scope.refreshException = function () {
     // Allow numbers only, must be applied every time
     $("input.form-control-number").numeric();
   };
   
-  $scope.basePayrollRates = function () {
+  $scope.addServiceException = function () {
+    $scope.serviceExceptions.push({ option: 'After Hours/Minutes in Range', hour: 40, BaseServiceRate: $scope.baseServiceRates[0], Date: new Date() });
+
+    // Trigger the alternate rate to set default
+    $scope.selectServiceAlternateRate($scope.serviceExceptions[$scope.serviceExceptions.length - 1]);
+
+    // Allow numbers only, must be applied every time
+    $("input.form-control-number").numeric();
+  };
+
+  /**
+   * Removes the payroll exception at the given index.
+   */
+  $scope.removePayrollException = function (idx) {
+    $scope.payrollExceptions.splice(idx, 1);
+  };
+  
+  /**
+   * Removes the service exception at the given index.
+   */
+  $scope.removeServiceException = function (idx) {
+    $scope.serviceExceptions.splice(idx, 1);
+  };
+
+  // $scope.refreshException = function () {
+  //   // Allow numbers only, must be applied every time
+  //   $("input.form-control-number").numeric();
+  // };
+  
+  /**
+   * Refreshes the list of base payroll rates from the server
+   * and adds a default payroll rate exception.
+   */
+  $scope.refreshBasePayrollRates = function () {
     $http.get($rootScope.baseUrl + "/odata/Rates/Default.BasePayrollRatesForPunches(InAt='" + $scope.formatMomentFromDate($rootScope.range.InAt, 'YYYY-MM-DD') + "',OutAt='" + $scope.formatMomentFromDate($rootScope.range.OutAt, 'YYYY-MM-DD') + "')?$count=true")
       .then(response => {
         $scope.basePayrollRates = response.data.value;
 
         // Add the first exception
-        $scope.addException();
+        $scope.addPayrollException();
       }, error => {
         console.error(error)
       });
   };
 
-  $scope.refreshAlternateRates = function () {
+  /**
+   * Refreshes the list of alternate payroll rates from the server.
+   */
+  $scope.refreshAlternatePayrollRates = function () {
     $http.get($rootScope.baseUrl + "/odata/Rates/Default.AlternatePayrollRatesForPunches(InAt='" + $scope.formatMomentFromDate($rootScope.range.InAt, 'YYYY-MM-DD') + "',OutAt='" + $scope.formatMomentFromDate($rootScope.range.OutAt, 'YYYY-MM-DD') + "')?$count=true")
       .then(response => {
         $scope.alternatePayrollRates = response.data.value;
@@ -45,20 +75,56 @@ app.controller('ModalPopulateRatesController', function ($http, $rootScope, $sco
       });
   };
 
-  $scope.selectAlternateRate = function (exception) {
+  /**
+   * Refreshes the list of base service rates from the server
+   * and adds a default service rate exception.
+   */
+  $scope.refreshBaseServiceRates = function () {
+    $http.get($rootScope.baseUrl + "/odata/Rates/Default.BaseServiceRatesForPunches(InAt='" + $scope.formatMomentFromDate($rootScope.range.InAt, 'YYYY-MM-DD') + "',OutAt='" + $scope.formatMomentFromDate($rootScope.range.OutAt, 'YYYY-MM-DD') + "')?$count=true")
+      .then(response => {
+        $scope.baseServiceRates = response.data.value;
+
+        // Add the first exception
+        $scope.addServiceException();
+      }, error => {
+        console.error(error)
+      });
+  };
+
+  /**
+   * Refreshes the list of alternate service rates from the server.
+   */
+  $scope.refreshAlternateServiceRates = function () {
+    $http.get($rootScope.baseUrl + "/odata/Rates/Default.AlternateServiceRatesForPunches(InAt='" + $scope.formatMomentFromDate($rootScope.range.InAt, 'YYYY-MM-DD') + "',OutAt='" + $scope.formatMomentFromDate($rootScope.range.OutAt, 'YYYY-MM-DD') + "')?$count=true")
+      .then(response => {
+        $scope.alternateServiceRates = response.data.value;
+      }, error => {
+        console.error(error)
+      });
+  };
+
+  $scope.selectPayrollAlternateRate = function (exception) {
     $timeout(function() {
       exception.AlternatePayrollRate = $scope.alternatePayrollRates[0];
+    }, 50);
+  };
+  
+  $scope.selectServiceAlternateRate = function (exception) {
+    $timeout(function() {
+      exception.AlternateServiceRate = $scope.alternateServiceRates[0];
     }, 50);
   };
 
   $scope.refreshTasks = function () {
     $http.get($rootScope.baseUrl + "/odata/Tasks/Default.ForPunches(InAt='" + $scope.formatMomentFromDate($rootScope.range.InAt, 'YYYY-MM-DD') + "',OutAt='" + $scope.formatMomentFromDate($rootScope.range.OutAt, 'YYYY-MM-DD') + "')?$count=true&$expand=BasePayrollRate,BaseServiceRate")
       .then(response => {
-        $scope.tasks = response.data.value
-        $scope.basePayrollRates();
-        $scope.refreshAlternateRates();
+        $scope.tasks = response.data.value;
+        $scope.refreshBasePayrollRates();
+        $scope.refreshAlternatePayrollRates();
+        $scope.refreshBaseServiceRates();
+        $scope.refreshAlternateServiceRates();
       }, error => {
-        console.error(error)
+        console.error(error);
       });
   };
   $scope.refreshTasks();
@@ -67,23 +133,34 @@ app.controller('ModalPopulateRatesController', function ($http, $rootScope, $sco
     $uibModalInstance.dismiss();
   };
 
+  /**
+   * Builds payload for populate rate options and sends them to the server.
+   */
   $scope.ok = function () {
-    $uibModalInstance.close();
+    // for (var i = 0; i < $scope.payrollExceptions.length; i++) {
+    //   console.log($scope.payrollExceptions[i]);
+    // }
+
+    // for (var i = 0; i < $scope.serviceExceptions.length; i++) {
+    //   console.log($scope.serviceExceptions[i]);
+    // }
+
+    // $uibModalInstance.close();
   };
 
   $scope.movePayrollExceptionUp = function (idx) {
     if (idx > 0) {
-      var tmp = $scope.exceptions[idx];
-      $scope.exceptions[idx] = $scope.exceptions[idx - 1];
-      $scope.exceptions[idx - 1] = tmp;
+      var tmp = $scope.payrollExceptions[idx];
+      $scope.payrollExceptions[idx] = $scope.payrollExceptions[idx - 1];
+      $scope.payrollExceptions[idx - 1] = tmp;
     }
   };
   
   $scope.movePayrollExceptionDown = function (idx) {
-    if (idx < $scope.exceptions.length - 1) {
-      var tmp = $scope.exceptions[idx];
-      $scope.exceptions[idx] = $scope.exceptions[idx + 1];
-      $scope.exceptions[idx + 1] = tmp;
+    if (idx < $scope.payrollExceptions.length - 1) {
+      var tmp = $scope.payrollExceptions[idx];
+      $scope.payrollExceptions[idx] = $scope.payrollExceptions[idx + 1];
+      $scope.payrollExceptions[idx + 1] = tmp;
     }
   };
 
